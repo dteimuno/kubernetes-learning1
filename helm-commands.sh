8f210ca11c5d6792ef2d97c5155c4028fcbb64ad
Dennis Tei-Muno <munodennis@gmail.com>
2025-03-15 21:14:09 -0500

    Create helm-commands.sh

---
+++ helm-commands.sh
@@ -0,0 +1,16 @@
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo list
+ helm list
+helm repo list
+helm --help
+helm list
+helm list
+helm  --help
+helm upgrade --help
+helm list
+helm upgrade dazzling-web bitnami/nginx --version=18.3.6
+helm list
+helm --help
+helm rollback --help
+helm list
+helm rollback dazzling-web 3
